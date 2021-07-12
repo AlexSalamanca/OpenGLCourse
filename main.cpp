4c2835575b92d47836bb2c2ce615635f1461dc98
Alejandro Salamanca <alexsalamanca94@hotmail.com>
2021-07-11 18:25:49 -0700

    Created the Vertex Shader and the Fragment Shader and drawed the first triangle to the screen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <iostream>
 
 #include <GL/glew.h>
@@ -7,6 +8,111 @@
 // Window Dimensions
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+// Vertex Shader
+static const char* vShader = "									\n\
+#version 330													\n\
+																\n\
+layout(location = 0) in vec3 pos;								\n\
+																\n\
+void main()														\n\
+{																\n\
+	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
+}";
+
+// Fragment Shader
+static const char* fShader = "									\n\
+#version 330													\n\
+																\n\
+out vec4 color;													\n\
+																\n\
+void main()														\n\
+{																\n\
+	color = vec4(1.0, 0.0, 0.0, 1.0);							\n\
+}";
+
+
+void CreateTrinagle() {
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+}
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType) {
+	GLuint theShader = glCreateShader(shaderType);
+
+	const GLchar* theCode[1];
+	theCode[0] = shaderCode;
+
+	GLint codeLength[1];
+	codeLength[0] = strlen(shaderCode);
+
+	glShaderSource(theShader, 1, theCode, codeLength);
+	glCompileShader(theShader);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+	if (!result) {
+		glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error compiling the " << shaderType << " shader:" << eLog << std::endl;
+		return;
+	}
+
+	glAttachShader(theProgram, theShader);
+
+}
+
+void CompileShaders() {
+	shader = glCreateProgram();
+
+	if (!shader) {
+		std::cout << "Error creating the shader program" << std::endl;
+		return;
+	}
+
+	AddShader(shader, vShader, GL_VERTEX_SHADER);
+	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+	if (!result) {
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error linking program:" << eLog << std::endl;
+		return;
+	}
+
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+	if (!result) {
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error validating program:" << eLog << std::endl;
+		return;
+	}
+
+}
 
 int main() {
 	// Inititalize GLFW
@@ -52,6 +158,9 @@ int main() {
 	// Setup viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
+	CreateTrinagle();
+	CompileShaders();
+
 	// Loop until window closed
 	while (!glfwWindowShouldClose(mainWindow)) {
 		// Get and handle user input events
@@ -60,6 +169,14 @@ int main() {
 		// Clear window
 		glClearColor(0.f, 0.f, 0.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+		glUseProgram(0);
 
 		glfwSwapBuffers(mainWindow);
 	}
