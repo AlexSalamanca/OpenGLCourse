e96f22677e91f0b1fd11f75ba866f798845975c4
Alejandro Salamanca <alexsalamanca94@hotmail.com>
2021-07-12 21:11:56 -0700

    Added Translation, Rotation, and Scalation to the triangle

--- main.cpp
+++ main.cpp
@@ -6,11 +6,17 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // Window Dimensions
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformXMove;
+const float toRadians = 3.14159265f / 180.0f;
+
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -23,11 +29,11 @@ static const char* vShader = "											\n\
 																		\n\
 layout(location = 0) in vec3 pos;										\n\
 																		\n\
-uniform float xMove;													\n\
+uniform mat4 model;														\n\
 																		\n\
 void main()																\n\
 {																		\n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);	\n\
+	gl_Position = model * vec4(pos, 1.0);				\n\
 }";
 
 // Fragment Shader
@@ -120,7 +126,7 @@ void CompileShaders() {
 		return;
 	}
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 
 }
 
@@ -188,7 +194,12 @@ int main() {
 
 		glUseProgram(shader);
 
-		glUniform1f(uniformXMove, triOffset);
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::rotate(model, 90 * toRadians, glm::vec3(0.0f, 0.f, 1.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
+		
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
